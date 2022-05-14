527ac0ef439cfa127f44d322e6465bd16cbf37b7
Macbook Air <macbookair@Macbook-Airs-MacBook-Air.local>
2022-05-14 22:50:40 +0900

    2주차 django 개발 시작

---
+++ mysite/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import MainContent
+
+admin.site.register(MainContent)
+
+# Register your models here.
---
+++ pages/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def mainpage(request):
+    return render(request, 'pages/mainpage.html')
+
+def company(request):
+    return render(request, 'pages/company_info.html')
+
+# Create your views here.
